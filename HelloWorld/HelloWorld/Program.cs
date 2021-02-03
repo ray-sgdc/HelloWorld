fb1fb56564851d80708c1eec0a08f74bc21fe972
sgdc-ray <raymond.ong@sg.yokogawa.com>
2021-02-03 18:15:28 +0800

    Changes in FeatureX branch

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -15,6 +15,7 @@ namespace HelloWorld
             Console.WriteLine("Hello World 3");
             Console.WriteLine("Hello World 4.5");
 			Console.WriteLine("Feature X");
+			Console.WriteLine("Feature X2");
             Console.ReadLine();
         }
     }
